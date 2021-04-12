c8b8448a9c27f2d338e1c692cc2df4d64d48ee4c
Nursultan <65071183+Nursultan-coder@users.noreply.github.com>
2021-04-13 00:44:31 +0600

    Update 08-remove-from-list.js

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -1,1 +1,15 @@
 
+function removeKFromList(/*l, k*/) {
+  // let result = l;
+  // while (l !== null) {
+  //   if (result.value === k) {
+  //     result.value = result.next.value;
+  //     result.next = result.next.next;
+  //   }
+  // }
+  // result = result.next;
+  // return l;
+  throw new Error('Not implemented');
+}
+
+module.exports = removeKFromList;
